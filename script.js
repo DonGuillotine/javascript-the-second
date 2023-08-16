2b0bceec4fad10e39de53c14add2c2671896a1b9
Donald Nwokoro <infect3dlab@gmail.com>
2023-08-16 13:14:13 +0100

    Coding Challange

--- script.js
+++ script.js
@@ -249,3 +249,37 @@ const chidubemAge = yearNow - 1990;
 
 // console.log(guysAge, chidubemAge, guysAge + chidubemAge);
 console.log((guysAge + chidubemAge) * 10);
+
+
+/* ------------- CODING CHALLANGE -> JavaScript Operators ---------------
+
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height ** 2 (mass in kg and height in meter).
+
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both versions)
+3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
+
+TEST DATA 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+
+*/
+
+// Step 1 - Store Mark and John's mass and height in variables
+let markMass, markHeight, johnMass, johnHeight, markBMI, johnBMI, markHigherBMI;
+markMass = 78;
+markHeight = 1.69;
+
+johnMass = 92;
+johnHeight = 1.95;
+
+
+// Step 2 - Mark and John's BMI using the formular: BMI = mass / height ** 2 
+markBMI = markMass / markHeight ** 2; // markMass / markHeight * markHeight
+johnBMI = johnMass / johnHeight ** 2;
+
+console.log(markBMI, johnBMI);
+
+
+// Step 3 - Create a boolean variable 'markHigherBMI'
+markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);

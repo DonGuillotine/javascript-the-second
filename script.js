ff1d6d9e0a267c59c8a195baf022d66b0a0bc76b
Donald Nwokoro <infect3dlab@gmail.com>
2023-08-23 15:17:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -510,7 +510,6 @@ Steven wants to build a very simple tip calculator for whenever he goes eating i
 TEST DATA: Test for bill values 275, 40 and 430
 
 HINT: To calculate 20% of a value, simply multiply it by 20/100 = 0.2
-HINT: Value X is between 50 and 300, if it's >= 50 && <= 300 😉
+HINT: Value X is between 50 and 300, if it's >= 50 && <= 300 
 
-GOOD LUCK 😀
 */

26fbbcea420ddd914233957e890ff6769102b11e
Donald Nwokoro <infect3dlab@gmail.com>
2023-08-28 21:20:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -534,5 +534,7 @@ HINT: To calculate 20% of a value, simply multiply it by 20/100 = 0.2
 HINT: Value X is between 50 and 300, if it's >= 50 && <= 300 
 
 const bill = 275;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
 
 */

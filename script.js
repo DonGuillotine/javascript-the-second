02f5ffda7bd9df22960a43efd9a1af31b91efaab
Donald Nwokoro <infect3dlab@gmail.com>
2023-08-28 21:20:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -535,6 +535,6 @@ HINT: Value X is between 50 and 300, if it's >= 50 && <= 300
 
 const bill = 275;
 const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value is ${bill + tip}`);
 
 */
